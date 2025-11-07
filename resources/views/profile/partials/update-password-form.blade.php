7b4ff411a8b216cd12973bc902433576e848e517
Brank01Dev <pavlovic.branko29@gmail.com>
2025-11-07 21:01:57 +0100

    Small update on Edit Profile .scss style.

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,15 +1,17 @@
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+        <h2>
             {{ __('Update Password') }}
         </h2>
 
-        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+        <p>
             {{ __('Ensure your account is using a long, random password to stay secure.') }}
         </p>
     </header>
 
-    <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
+    <div style="margin-right:100%">
+
+    <form method="post" action="{{ route('password.update') }}">
         @csrf
         @method('put')
 
@@ -40,9 +42,9 @@
                     x-show="show"
                     x-transition
                     x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400"
                 >{{ __('Saved.') }}</p>
             @endif
         </div>
     </form>
+    </div>
 </section>
